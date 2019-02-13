7a521943c0ffc12fc3b1e7e9acec54c2a3b7616a
Taner Akyil <tano_1907@hotmail.com>
2019-02-13 14:37:19 +0300

    Wissen

--- Wissen/Controllers/HomeController.cs
+++ Wissen/Controllers/HomeController.cs
@@ -26,5 +26,33 @@ namespace Wissen.Controllers
 
             return View();
         }
+        [HttpPost]
+        public ActionResult Contact(string firstName,string lastName,string email,string phone,string department,string message)
+        {
+            System.Net.Mail.MailMessage mailMessage = new System.Net.Mail.MailMessage();
+            mailMessage.From = new System.Net.Mail.MailAddress("tanerakyil@gmail.com", "tano");
+            mailMessage.Subject = "İletişim Formu: " + firstName+ " " + lastName;
+            mailMessage.To.Add("tanerakyil@gmail.com,tanerakyil@gmail.com");
+            string body;
+            body = "Ad: " + firstName + "<br />";
+            body = "Soyad: " + lastName + "<br />";
+            body += "Telefon: " + phone + "<br />";
+            body += "E-posta: " + email + "<br />";
+            body += "Depart: " + department + "<br />";
+            body += "Mesaj: " + message + "<br />";
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Body = body;
+
+          
+            System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new System.Net.NetworkCredential("mail", "sifre");
+            smtp.EnableSsl = true;
+            smtp.Send(mailMessage);
+
+            ViewBag.Message = "Mesajınız gönderildi. Teşekkür ederiz.";
+            //TODO: Mail Gönderme işlemi yapılacak.
+            ViewBag.Message = "Form başarıyla iletildi,en kısa zamanda dönüş yapacağız.";
+            return View();
+        }
     }
 }
